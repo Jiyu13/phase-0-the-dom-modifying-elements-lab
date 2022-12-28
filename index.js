428af364a51fad6a0767e98611205ac3e019b3d1
Ziru Zou <jiyucoding@yahoo.com>
2022-12-28 15:34:58 -0500

    commit tree b07237083ebb1e9eebc40e4510067f7cb5f23690

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+// Make sure you remove the <main> with id 'main'
+const main = document.getElementById("main");
+main.remove()
+
+// Make sure you create an <h1> and assign it to the 'newHeader' variable
+const newHeader = document.createElement("h1");
+
+// /the 'newHeader' variable that points to the <h1> node has an id of 'victory'
+newHeader.id = "victory";
+
+// Make sure your <h1> with id 'victory' includes the text "YOUR-NAME is the champion" inside it
+newHeader.textContent = "Sam is the champion";
+
+// document.body.append(newHeader);
